af1b8eda5df4c56f1ee44a8cad148ec00a2a2206
pab <pablosaavedra123@gmail.com>
2022-04-10 22:24:09 -0300

    No more creation of temporal file

--- hello_world_bug.c
+++ hello_world_bug.c
@@ -5,30 +5,21 @@ static PerlInterpreter *my_perl;
 
 int main(int argc, char **argv, char **env) {
 
-    char *args[] = { NULL };
-    FILE *fp;
-
-    argc = 2;
-    argv[1] = "temp.pl";
-
-    fp = fopen("./temp.pl", "w");
-    fprintf(fp, "sub foo{print \"Hello World!\\n\"}");
-    fclose(fp);
+    char *embedding[] = { "", "-e", "0", NULL };
 
     PERL_SYS_INIT3(&argc, &argv, &env);
     my_perl = perl_alloc();
     perl_construct(my_perl);
 
-    perl_parse(my_perl, NULL, argc, argv, NULL);
+    perl_parse(my_perl, NULL, 3, embedding, NULL);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+    perl_run(my_perl);
 
-    call_argv("foo", G_DISCARD | G_NOARGS, args);
+    eval_pv("print \"Hello World!\n\"", TRUE);
 
     perl_destruct(my_perl);
     perl_free(my_perl);
     PERL_SYS_TERM();
 
-    remove("./temp.pl");
     exit(EXIT_SUCCESS);
 }
-
